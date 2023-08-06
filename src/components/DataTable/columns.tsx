15a942817b92440a7696750b90831a71d6526daf
Davi Rezende <daviresendes12@gmail.com>
2023-08-05 23:23:41 -0300

    fix: eslint

--- src/components/DataTable/columns.tsx
+++ src/components/DataTable/columns.tsx
@@ -1,16 +1,5 @@
 'use client'
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from '@radix-ui/react-dropdown-menu'
 import { ColumnDef } from '@tanstack/react-table'
-import { MoreHorizontal } from 'lucide-react'
-import { Button } from 'react-day-picker'
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
@@ -22,34 +11,6 @@ export type Payment = {
 }
 
 export const columns: ColumnDef<Payment>[] = [
-  // {
-  //   id: 'actions',
-  //   cell: ({ row }) => {
-  //     const payment = row.original
-
-  //     return (
-  //       <DropdownMenu>
-  //         <DropdownMenuTrigger asChild>
-  //           <Button className="h-8 w-8 p-0">
-  //             <span className="sr-only">Open menu</span>
-  //             <MoreHorizontal className="h-4 w-4" />
-  //           </Button>
-  //         </DropdownMenuTrigger>
-  //         <DropdownMenuContent align="end">
-  //           <DropdownMenuLabel>Actions</DropdownMenuLabel>
-  //           <DropdownMenuItem
-  //             onClick={() => navigator.clipboard.writeText(payment.id)}
-  //           >
-  //             Copy payment ID
-  //           </DropdownMenuItem>
-  //           <DropdownMenuSeparator />
-  //           <DropdownMenuItem>View customer</DropdownMenuItem>
-  //           <DropdownMenuItem>View payment details</DropdownMenuItem>
-  //         </DropdownMenuContent>
-  //       </DropdownMenu>
-  //     )
-  //   },
-  // },
   {
     accessorKey: 'status',
     header: 'Status',
